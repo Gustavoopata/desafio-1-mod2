f7b256ef9c0f8ea13990265c0c7fa237e6a320d7
Gustavoopata <gustavobimbinha24@gmail.com>
2023-09-05 13:04:40 -0300

    desafio 1 mod 2

--- script.js
+++ script.js
@@ -69,11 +69,90 @@ function verificarVerdadeiroOuFalso(variavel) {
   }
   verificarVerdadeiroOuFalso(true);
   
-//   20
+// 9-13
+
+var suplementos = ["creatina", "cafeina", "whey protein", "pré treino", "bca"]
+
+suplementos.unshift("Durateston")
+
+suplementos.pop()
+
+suplementos.push("colageno", "hipercalorico")
+
+suplementos.shift()
+
+console.log(suplementos)
   
-  
+//   14
+
+let numeros = [7, 5, 6, 3, 8, 9, 2, 1, 4];
 
 
+numeros.sort(function(a, b) {
+  return a - b;
+});
+
+
+console.log(numeros);
+
+// 15-18
+
+var meuPerfil = {
+
+    nome: "Gustavo Opata",
+    idade: 17 ,
+    cidade: "São Paulo"
+}
+
+
+meuPerfil.hobby = "Academia"
+
+delete meuPerfil.idade;
+
+for (var propriedade in meuPerfil) {
+    console.log(propriedade + ": " + meuPerfil[propriedade])
+  }
+
+console.log(meuPerfil)
+
+// 19
+
+var cadastro = [
+    {
+      nome: "João Vitor",
+      idade: 30,
+      telefone: "(11)96786-7890",
+      amigos: ["Maria", "Pedro", "Ana", "Carlos"]
+    },
+    {
+      nome: "Maria Eduarda",
+      idade: 28,
+      telefone: "(13)97654-3210",
+      amigos: ["João", "Pedro", "Lucas", "Lúcia"]
+    },
+    {
+      nome: "Pedro Henrique",
+      idade: 32,
+      telefone: "(21)95123-4567",
+      amigos: ["João", "Maria", "Lucas", "Rafael"]
+    },
+    {
+      nome: "Nathan Cavalcante",
+      idade: 25,
+      telefone: "(33)93789-0123",
+      amigos: ["Lucas", "Carla", "Rafael", "Paula"]
+    },
+    {
+      nome: "Lucas Lima",
+      idade: 28,
+      telefone: "(77)97888-9999",
+      amigos: ["Pedro", "Ana", "Rafael", "Marta"]
+    }
+  ];
   
+
+  cadastro.forEach(function(pessoa) {
   
+    console.log(pessoa.nome + " tem o amigo " + pessoa.amigos[0]);
+  });
   
