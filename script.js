72d9eba54191a200643955bf9b12d17e95ee92d7
Gustavoopata <gustavobimbinha24@gmail.com>
2023-09-05 13:04:07 -0300

    desafi 1 mod 2

--- script.js
+++ script.js
@@ -1,8 +1,79 @@
+// 1
 
-var dia = ""
+let dia = "noite"
 
-if (dia) {
-    console.log('claro')
+if(dia == "dia"){
+    console.log("claro")
 } else {
-    console.log('de noite')
+console.log("de noite")
 }
+
+// 2
+let contador = 0
+
+while(contador < 20){
+    console.log(contador += 2)
+}
+
+// 3
+function exibirMensagem() {
+    console.log("Desafio ta dicil em");
+  }
+  
+  exibirMensagem();
+
+//4
+function exibirNome(nome) {
+    console.log(nome)
+}
+
+exibirNome("Gustavo Opata")
+
+// 5
+
+function exibirInformacoes(nome, idade, estiloMusical) {
+    console.log("Nome: " + nome);
+    console.log("Idade: " + idade);
+    console.log("Estilo Musical : " + estiloMusical);
+  }
+  
+
+  exibirInformacoes("Gustavo Opata", 17, "Funk");
+
+//   6
+
+function exibirFilmeEMusica(filme, musica) {
+    console.log("Filme: " + filme);
+    console.log("Música: " + musica);
+  }
+  
+  exibirFilmeEMusica("Up!", "Fernando de Noronha");
+
+//   7
+
+function triplo(primeiroValor){
+    console.log(primeiroValor * 3 )
+}
+triplo(7)
+
+// 8
+
+function verificarVerdadeiroOuFalso(variavel) {
+    if (variavel === true) {
+      console.log("A variável é verdadeira.");
+    } else if (variavel === false) {
+      console.log("A variável é falsa .");
+    } else {
+      console.log("A variável não é true nem false.");
+    }
+  }
+  verificarVerdadeiroOuFalso(true);
+  
+//   20
+  
+  
+
+
+  
+  
+  
